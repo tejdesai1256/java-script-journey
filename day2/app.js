eb922d38b73e650f7d94f2592eae4162c9e7a9c2
Tej Desai <someone@gmail.com>
2026-03-17 18:21:06 +0530

    Updated Day2 JavaScript journey

---
+++ day2/app.js
@@ -0,0 +1,28 @@
+// let month = 4; 
+//       switch (month) { 
+//         case 1: 
+//           console.log("january"); 
+//           break; 
+//         case 2: 
+//           console.log("february"); 
+//           break; 
+//         case 3: 
+//           console.log("march"); 
+//           break; 
+
+//           case 4:
+//             console.log("april");
+//             break;
+//         default: 
+//           console.log("no month");}
+
+
+// function add(a,b){
+//     console.log(a+b);
+// }
+// add(10,30);
+
+//template literals
+
+// let str=`this is template literals ${1+4+8} ruppes`;
+// console.log(str);
