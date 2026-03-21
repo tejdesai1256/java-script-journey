1319c2e246f94e6988817f77f285ad81212e7d32
Tej Desai <someone@gmail.com>
2026-03-21 23:10:08 +0530

    Array Started

---
+++ Array/app.js
@@ -0,0 +1,8 @@
+// let student=["tej","raj","rohit","ashish","ganesh","vishal"];
+// console.log(student)
+// for(let i=0;i<=student.length;i++){
+//     console.log(i,student[i])
+// }
+
+let arr=['jan','july','mar','aug']
+console.log(arr)
--- day2/app.js
+++ day2/app.js
@@ -74,19 +74,101 @@
 // }
 
 // let size=prompt("enter the size");
-let size="l";
-if(size=="s"){
-    console.log("50")
-}
-else if(size=="m"){
-    console.log("100")
-}
-else if(size=="l"){
-    console.log("200")
-}
-else if(size=="xl"){
-    console.log("250")
-}
-else{
-    console.log("size not available")
-}
+// let size="l";
+// if(size=="s"){
+//     console.log("50")
+// }
+// else if(size=="m"){
+//     console.log("100")
+// }
+// else if(size=="l"){
+//     console.log("200")
+// }
+// else if(size=="xl"){
+//     console.log("250")
+// }
+// else{
+//     console.log("size not available")
+// }
+
+// let str="banaana";
+// if(str[0]=="a" && str.length>3){
+//     console.log("Good String");
+// }
+// else{
+//     console.log("Bad String");
+// }
+
+//switch case
+// 
+
+//alerts
+
+// let a= prompt("enter first number")
+// let b= prompt("enter second number")
+// console.log("Addition Is "+(a+b))
+
+// let num=100;
+// if(num%10==0){
+//     console.log("number is divisible by 10")
+// }
+// else{
+//     console.log("number is not divisible")
+// }
+
+// let name=prompt("enter your name")
+// let age=prompt("enter your age")
+// alert(`name is ${name} and age is ${age}`)
+
+// let querter=3
+// switch(querter){
+// case 1:
+//     console.log("jan feb mar apr")
+//     break;
+
+//     case 2:
+//     console.log("may jun jul aug")
+//     break;
+
+//     case 3:
+//     console.log("sep oct nov dec")
+//     break;
+
+//    default:
+//     console.log("enter valid querter")
+// }
+
+
+// let str="pineapple"
+// if(str[0]=='a' & str.length==5){
+//     console.log("Golden string")
+// }
+// else{
+//     console.log(" Not Golden string")
+// }
+
+//largest of three number
+// let a=110
+// let b=20
+// let c=30
+
+// if(a>b && a>c){
+//     console.log("a is grater")
+
+// }
+// else if(b>a && b>c){
+//     console.log("b is greater")
+// }
+// else{
+//     console.log("c is greater")
+// }
+
+// let num1=2343
+// let num2=5643
+// if(num1[-1]==num2[-1]){
+//     console.log("same digit")
+// }
+// else{
+//     console.log("not same")
+// }
+
