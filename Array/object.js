de8301e7ef729390d058274e4483573a877defdb
tejdesai1256 <desaitej1256@gmail.com>
2026-07-23 20:05:15 +0530

    objects using new keyword

--- Array/object.js
+++ Array/object.js
@@ -1,7 +1,15 @@
-// Create an Object
-const person = {
+// // Create an Object
+// const person = {
+//   firstName: "John",
+//   lastName: "Doe",
+//   age: 50,
+//   eyeColor: "blue"
+// };
+
+// Create an Object using new keyword
+const person = new Object({
   firstName: "John",
   lastName: "Doe",
   age: 50,
   eyeColor: "blue"
-};
+});
